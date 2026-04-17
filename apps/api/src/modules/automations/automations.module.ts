f7e05662a6810b2774ede3efd05d91fa5fc0cf5d
CRM Dev <dev@idealhost.cloud>
2026-04-17 10:03:07 +0300

    Fix AutomationsModule: import NotificationsModule + EmailsModule for DI

--- apps/api/src/modules/automations/automations.module.ts
+++ apps/api/src/modules/automations/automations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AutomationsController } from './automations.controller';
 import { AutomationsService } from './automations.service';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { EmailsModule } from '../emails/emails.module';
 
 @Module({
+  imports: [NotificationsModule, EmailsModule],
   controllers: [AutomationsController],
   providers: [AutomationsService],
   exports: [AutomationsService],
